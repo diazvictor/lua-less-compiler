e87e26050e4aab9772e89cfc2dee2ccf4ff335a2
diazvictor <diaz.victor@openmailbox.org>
2018-09-12 23:47:51 -0400

    añadida la funcion os.capture

--- init.lua
+++ init.lua
@@ -43,6 +43,18 @@ local setMinify = builder:get_object('setMinify')
 local compile = builder:get_object('compile')
 
 --------------------------------------------------------------------------------
+function os.capture(cmd, raw)
+	local f = assert(io.popen(cmd, 'r'))
+	local s = assert(f:read('*a'))
+	f:close()
+
+	if raw then return s end
+	s = string.gsub(s, '^%s+', '')
+	s = string.gsub(s, '%s+$', '')
+	s = string.gsub(s, '[\n\r]+', ' ')
+	return s
+end
+
 
 --------------------------------------------------------------------------------
 
